b467987ae16d069f6b1e2548d3493733f201338d
bworthin <brandy.worthington@gmail.com>
2017-04-18 15:24:05 -0400

    renamed stars variable to yelp_avg_rating

--- Yelp_business_clean.R
+++ Yelp_business_clean.R
@@ -12,10 +12,12 @@ business_tbl <- as_data_frame(business)
 
 #filter to businesses with >=100 reviews, with "Restaurants" as part of their category attribute
 #remove unneeded columns/variables: hours, attributes, lat & long, neigh, address, open, type, categories
+#rename starts column to 'yelp_avg_rating'
 business_tbl_filt <- business_tbl %>%
   filter(review_count >= 100, str_detect(categories, "Restaurants")) %>%
   select(-starts_with("hours"), -starts_with("attributes"), -latitude, -longitude, 
-             -neighborhood, -address, -is_open, -type, -categories) 
+             -neighborhood, -address, -is_open, -type, -categories) %>%
+  rename(yelp_avg_rating = stars)
   
    
 
