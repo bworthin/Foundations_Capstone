b193285a8db46b16d209446d913e835f603560a9
bworthin <brandy.worthington@gmail.com>
2017-04-19 18:16:13 -0400

    added custom handler to filter content from file

--- Wrangling/Yelp_business_clean.R
+++ Wrangling/Yelp_business_clean.R
@@ -6,22 +6,32 @@ library(tidyr)
 library(dplyr)
 library(stringr)
 
-business <- stream_in(file("yelp_academic_dataset_business.json"))
+#streamin with handler function to bring in specified variables for businesses with >=100 reviews, 
+#and category "Restaurant"; stream out result to temp file, then assign to 'business' object
+
+con_in <- file("yelp_academic_dataset_business.json")
+con_out <- file(tmp <-tempfile(), open = "wb")
+stream_in(con_in, handler = function(df) {
+  df <- df %>%
+    select(business_id, stars, review_count, categories) %>%
+    filter(review_count >= 100, str_detect(categories, "Restaurants")) %>%
+    stream_out(con_out, pagesize = 1000)
+}, pagesize = 1000)
+close(con_out)
+
+business <- stream_in(file(tmp))
+
 business_tbl <- as_data_frame(business)
 
 
-#filter to businesses with >=100 reviews, with "Restaurants" as part of their category attribute
-#remove unneeded columns/variables: hours, attributes, lat & long, neigh, address, open, type, categories
-#rename starts column to 'yelp_avg_rating'
+#remove categories variable, since we don't need it anymore; then rename stars to yelp_avg_rating
 business_tbl_filt <- business_tbl %>%
-  filter(review_count >= 100, str_detect(categories, "Restaurants")) %>%
-  select(-starts_with("hours"), -starts_with("attributes"), -latitude, -longitude, 
-             -neighborhood, -address, -is_open, -type, -categories) %>%
-  rename(yelp_avg_rating = stars)
+  select(-categories) %>%
+  rename(yelp_avg_rating = stars) 
 
 #write to new csv file (any reason to write as new json instead? simple format now)
 yelp_business_clean <- business_tbl_filt
-write.table(yelp_business_clean, "~/Foundations_Capstone/yelp_business_clean.csv", row.names = FALSE, sep = ",")
+write.table(yelp_business_clean, "~/Foundations_Capstone/Wrangling/yelp_business_clean.csv", row.names = FALSE, sep = ",")
 
    
 
